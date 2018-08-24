eec598aa3d3f0981e787f18ceb5202abb5536214
doris <dianafoxernesto@gmail.com>
2018-08-23 21:44:30 -0500

    mejorando mapas

--- src/pages/README.md
+++ src/pages/README.md
@@ -1,3 +1,8 @@
 # Pages
 
 Each page type available has a corresponding README, take a look by navigating to a page directory above.
+
+Para añadir paginas usamos el comando
+````
+ionic g page <Nombre_pagina>
+```
--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -17,6 +17,8 @@ export class MapaPage {
   myLocation;
   map;
   marker;
+  coordenadas;
+  coord;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -28,9 +30,20 @@ export class MapaPage {
   initMap() {
     
     this.myLocation = {lat: 4.646412, lng: -74.077778};
+    this.coordenadas=[];
+    this.coord=[{lat:4.603899,lng: -74.067230},
+      {lat:4.609375,lng: -74.082079},
+      {lat:4.613610,lng: -74.066372},
+      {lat:4.581586,lng: -74.080585},
+      {lat:4.556693,lng: -74.112414},
+      {lat:4.521438,lng: -74.089390},
+      {lat:4.550937,lng: -74.141717}]
+  
+  
+  
     console.log(this.myLocation)
     this.map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 25,
+      zoom: 10,
       center: this.myLocation,
       mapTypeId: 'satellite'
     });
@@ -42,6 +55,17 @@ export class MapaPage {
       position: this.myLocation
     });
     this.marker.addListener('click', this.toggleBounce.bind(this));
+
+    /*this.coord.forEach((punto)=>{
+      this.addMarker(punto);
+    });*/
+    let bounds = new google.maps.LatLngBounds();
+    this.coord.forEach((punto) => {
+      this.addMarker(punto);
+      bounds.extend(punto);
+      
+    })
+    this.map.fitBounds(bounds);
   }
   toggleBounce() {
     if (this.marker.getAnimation() !== null) {
@@ -50,5 +74,22 @@ export class MapaPage {
       this.marker.setAnimation(google.maps.Animation.BOUNCE);
     }
   }
+  addMarker(coord){
+    var infoWindow =  new google.maps.InfoWindow({
+      content: 'ubicado en: '+coord.lat
+    });
+    let internalMarker= new google.maps.Marker({
+      map: this.map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: coord
+    });
+    internalMarker.addListener('click',()=>{
+      infoWindow.open(this.map,internalMarker);  
+    });
+    console.log(internalMarker);
+    this.coordenadas.push(internalMarker);
+  }
+  
 
 }
